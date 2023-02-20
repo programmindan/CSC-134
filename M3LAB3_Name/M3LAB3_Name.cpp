5368f72f59058d8d27ec285d581a338ce7126134
programmindan <125482202+programmindan@users.noreply.github.com>
2023-02-20 11:14:12 -0500

    Add files via upload

--- M3LAB3_Name/M3LAB3_Name.cpp
+++ M3LAB3_Name/M3LAB3_Name.cpp
@@ -1,77 +1,105 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>    // Enables use of time() function
+using namespace std;
+
 // CSC 134
-// M3LAB3
+// M3LAB3 - Craps (version 2)
 // Name
-// 15 February 23
+// 20 February 2023
 
 /*
 This program simulates a single game of craps.
 First version (MVP) is a single roll.
+
+*/
+/*
+int question_one() {
+  int num = 0;
+  cout << "Question One goes here" << endl;
+  return 0;
+}
 */
 
-#include <iostream>
-#include <cstdlib>
-#include <ctime>                // Enables use of time() function
+int main() {
+  // variables
+  int die1, die2, roll;   // roll is 2d6
+  int seed;               // will use time(0) later
+  //char any_key;           // for cin.get()
 
-using namespace std;
+  // Question 1
 
-int main()
-{
-    // Variables
-    int die1, die2, roll;                                   // Roll is 2d6
-    int seed;                                               // Will use time(0) later
-    char any_key;                                           // For cin.get()
-    bool winner = false;
-    cout << "Welcome to the craps table!\n";
+  /*cout << "Question 1" << endl;
+  //question_one(); // run question one
+  int num = 1;
 
-    // For testing, we'll use a repeatable seed
-    cout << "What's your lucky number? ";
-    cin >> seed;
-    srand(seed);
+  // Question 2
+  cout << "Question 2" << endl;
+*/
+  bool winner = false;
+  cout << "Welcome to the craps table!\n";
+/*
+  // For testing, we'll use a repeatable seed
+  cout << "do you like dice games? " ;
+  string answer;
+  cin >> answer;
+  if (answer =="yes") {
+    cout << "Great, let's roll the bones!"<< endl;
+  }
+  else {
+    cout << "Sorry, I don't know how to do card games in c++ yet" << endl;
+  }
+  */
+  //cout << "What's your lucky number? ";
+  //cin >> seed;
+  // Seed is truly random
+  seed = time(0);
+  cout << "DEBUG: time(0) = " << seed << endl;
+  srand(seed);
 
-    // Start game
-    cout << "Let's roll! (press ENTER)" << endl;
-    // cin >> any_key;                                            // Ignore this, just looking for EENTER
-    die1 = (rand() % 6) + 1;                                      // From 1 to 6
-    die2 = (rand() & 6) + 1;                                      // From 1 to 6
-    roll = die1 + die2;
-    // Debug: just use luck number as roll
-    roll = seed; // take this out
+  // start game
+  cout << "Let's roll! " << endl; // TODO: 'press any key'
+  die1 = (rand() % 6) + 1;   // from 1 to 6
+  die2 = (rand() % 6) + 1;
+  roll = die1 + die2;
+  // DEBUG: just use lucky number as roll
+  // roll = seed; // take this out!
 
-    cout << "You rolled: " << die1 << " + " << die2 << " ) -> ";
-    cout << roll << endl;
+  cout << "You rolled: (" << die1 << " + " << die2 << ") -> ";
+  cout << roll << endl;
 
-    // Determine outcome
-    switch (roll) {
-        // instant losses
-        case 2:
-            cout << "Snake eyes!" << endl;
-            winner = false;
-            break;
-        case 12:
-            cout << "Boxcars!" << endl;
-            winner = false;
-            break;
+  // determine outcome!
+  switch (roll) {
+    //  instant losses
+    case 2:
+      cout << "Snake eyes!" << endl;
+      winner = false;
+      break;
+    case 12:
+      cout << "Boxcars!" << endl;
+      winner = false;
+      break;
 
-        // Instant wins
-        case 7:
-        cout << "You win!" << endl;
-        winner = true;
-        break;
+    // instant wins
+    case 7:
+      cout << "You win!" << endl;
+      winner = true;
+      break;
+    // TODO: add 11
 
-        // To be continued (point roll)
-        default:
-            cout << "Unknown - TODO" << endl;
-    }
-    if (winner) {
+    // to be continued (point rolls)
+    default:
+      cout << "Rolled: " << roll << endl;
+      cout << "Continue playing, point is " << roll << endl;
+      cout << "We'll just call it a draw" << endl;
+      break;
+  }
+  if (winner) {
     cout << "You won!" << endl;
-    }
-    else {
-        cout << "Sorry -- you didn't win" << endl;
-    }
-
-    // End game
-    cout << "Thank you for playing!" << endl;
-
-
-    return 0;
+  } else {
+    cout << "Sorry -- you didn't win" << endl;
+  }
+  // end game
+  cout << "Thank you for playing!" << endl;
+  return 0;
 }
