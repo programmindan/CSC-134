6447b9f971bfc3c0058dccf8d53bee0c4535f7a3
programmindan <125482202+programmindan@users.noreply.github.com>
2023-02-27 11:10:58 -0500

    Started project

---
+++ M4T2_Name/main.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M4T2 - Menus
+// Name
+// 27 February 2023
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // Print three line menu
+    // Press 1 to blah etc
+    // cin an integer
+
+    return 0;
+}
