3636cb6e6e53e07dc3aa3ddbd798e0838ee788c3
programmindan <125482202+programmindan@users.noreply.github.com>
2023-02-27 23:25:02 -0500

    Update M2LAB1_Name.cpp

--- M2LAB1_Name/M2LAB1_Name.cpp
+++ M2LAB1_Name/M2LAB1_Name.cpp
@@ -17,53 +17,53 @@ using namespace std;
 int main(){
 
 // Dimension & area variables
-double length;																					// Lenght input from user in ft
-double width;																					// Width input from user in ft
-double height;																					// Height input from user in ft
-double volume;																					// Volume = L * W * H in cubic ft
+double length;																					    // Lenght input from user in ft
+double width;																					      // Width input from user in ft
+double height;																					    // Height input from user in ft
+double volume;																					    // Volume = L * W * H in cubic ft
 
 // Cost & profit variables
 double WHOLESALE_PRICE;																			// Cost to produce crate in $
-double RETAIL_PRICE;																			// Price crate is sold at in $
-double profit;																					// Profit from crate sold in $
-const double WHOLESALE_PRICE_PER_CUBICFT = 0.23;												// Wholesale price of crate material in $ per cubic ft
-const double RETAIL_PRICE_PER_CUBICFT = 0.50;													// Retail price of crate material in $ per cubic ft
+double RETAIL_PRICE;																			  // Price crate is sold at in $
+double profit;																					    // Profit from crate sold in $
+const double WHOLESALE_PRICE_PER_CUBICFT = 0.23;						// Wholesale price of crate material in $ per cubic ft
+const double RETAIL_PRICE_PER_CUBICFT = 0.50;								// Retail price of crate material in $ per cubic ft
 
-cout << setprecision(2) << fixed << showpoint;													// Sets outputs to two decimal places
+cout << setprecision(2) << fixed << showpoint;							// Sets outputs to two decimal places
 
 // Output/Input to gather data for volume calculation
 cout << "Enter the crate dimensions in feet: \n";
 cout << "Lenght: ";
-cin >> length;																					// User input for length in ft
+cin >> length;																					  // User input for length in ft
 cout << "Width: ";
-cin >> width;																					// User input for width in ft
+cin >> width;																					    // User input for width in ft
 cout << "Height: ";
-cin >> height;																					// User input for height in ft
+cin >> height;																					  // User input for height in ft
 
 // Calculation for volume of crate
 volume = length * width * height;
 
 // Volume information output to user
-cout << "The volume of the crate is " << volume;												// Volume display
+cout << "The volume of the crate is " << volume;												    // Volume display
 cout << " cubic feet.\n";
 
 // Production cost calculation
-WHOLESALE_PRICE = volume * WHOLESALE_PRICE_PER_CUBICFT;											// Wholesale price calculation using cubic ft
+WHOLESALE_PRICE = volume * WHOLESALE_PRICE_PER_CUBICFT;										 // Wholesale price calculation using cubic ft
 
 // Production cost output to user
-cout << "Production cost: $" << WHOLESALE_PRICE << endl;										// Display production cost in $
+cout << "Production cost: $" << WHOLESALE_PRICE << endl;									 // Display production cost in $
 
 // Retail sale price calculation
-RETAIL_PRICE = volume * RETAIL_PRICE_PER_CUBICFT;												// Retail price calculation using cubic ft
+RETAIL_PRICE = volume * RETAIL_PRICE_PER_CUBICFT;												   // Retail price calculation using cubic ft
 
 // Retail price output to user
-cout << "Retail price: $" << RETAIL_PRICE << endl;												// Display retail cost in $
+cout << "Retail price: $" << RETAIL_PRICE << endl;												 // Display retail cost in $
 
 // Profit calculation
-profit = RETAIL_PRICE - WHOLESALE_PRICE;														// Profit in $
+profit = RETAIL_PRICE - WHOLESALE_PRICE;														       // Profit in $
 
 // Profit margin output to user
-cout << "Profit: $" << profit << endl;															// Display profit margin in $
+cout << "Profit: $" << profit << endl;															       // Display profit margin in $
 
 return 0;
 }
