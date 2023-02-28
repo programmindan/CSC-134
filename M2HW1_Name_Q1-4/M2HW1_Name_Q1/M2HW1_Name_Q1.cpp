984d91aec3548259216b0fa328e0bf13a3e6dc2f
programmindan <125482202+programmindan@users.noreply.github.com>
2023-02-27 23:19:03 -0500

    Update M2HW1_Name_Q1.cpp

--- M2HW1_Name_Q1-4/M2HW1_Name_Q1/M2HW1_Name_Q1.cpp
+++ M2HW1_Name_Q1-4/M2HW1_Name_Q1/M2HW1_Name_Q1.cpp
@@ -14,13 +14,13 @@ using namespace std;
 int main(){
 
 	// Variables for account & user information
-	string FIRST_NAME;																		// Variable for first name
-	string LAST_NAME;																		// Variable for last name
+	string FIRST_NAME;																	// Variable for first name
+	string LAST_NAME;																	// Variable for last name
 	double ACCOUNT_BALANCE;																	// Variable for starting account balance in $
-	long long ACCOUNT_NUMBER = 654784932018;												// Variable for account number 8 digits
+	long long ACCOUNT_NUMBER = 654784932018;														// Variable for account number 8 digits
 	double ACCOUNT_DEPOSIT;																	// Variable for account balance in $
 	double ACCOUNT_WITHDRAWAL;																// Variable for account withdrawal amount in $
-	double ACCOUNT_FINAL_BALANCE;															// Variable for final account balance in $
+	double ACCOUNT_FINAL_BALANCE;																// Variable for final account balance in $
 
 	// Sets outputs to 2 decimal points
 	cout << setprecision(2) << fixed << showpoint;
@@ -46,7 +46,7 @@ int main(){
 	// Account information output to user
 	cout << "Account Holder Name: " << FULL_NAME << endl;									// Displays account holder name
 	cout << "Account Number: " << ACCOUNT_NUMBER << endl;									// Displays account number
-	cout << "Final Account Balance: " << ACCOUNT_FINAL_BALANCE << endl;						// Displays final account balance
+	cout << "Final Account Balance: " << ACCOUNT_FINAL_BALANCE << endl;							// Displays final account balance
 
 	return 0;
 }
